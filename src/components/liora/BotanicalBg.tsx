27a1594702554300316b8dd23753c2b8f01e9d21
PB-DigitalDesigns <paulabenitez808@icloud.com>
2026-07-20 14:23:45 -0400

    Adjust left position of BotanicalBg component

--- src/components/liora/BotanicalBg.tsx
+++ src/components/liora/BotanicalBg.tsx
@@ -28,7 +28,7 @@ export function BotanicalBg({ className = "" }: { className?: string }) {
         className="
         hidden lg:block
         absolute
-        -left-[-20px]
+        -left-[-80px]
         top-[420px]
         w-[700px]
         rotate-[150deg]
