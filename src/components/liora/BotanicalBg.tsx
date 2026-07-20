77269d5021bec5b40ee332479980bebc9e0982fe
PB-DigitalDesigns <paulabenitez808@icloud.com>
2026-07-20 12:08:58 -0400

    Fix JSX syntax in BotanicalBg component

--- src/components/liora/BotanicalBg.tsx
+++ src/components/liora/BotanicalBg.tsx
@@ -3,9 +3,8 @@ import lily from "@/assets/lily-line-art.png";
 export function BotanicalBg({ className = "" }: { className?: string }) {
   return (
     <div
-      <div
-        className={`pointer-events-none absolute top-0 left-0 w-full h-[1000px] overflow-visible ${className}`}
-      >    
+      className={`pointer-events-none absolute top-0 left-0 w-full h-[1000px] overflow-visible ${className}`}
+     >    
       
       {/* Desktop Left */}
       <img
