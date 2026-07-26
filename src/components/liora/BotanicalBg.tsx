3033ecae501eea87fbca3ac1d26184f3ef4983e4
PB-DigitalDesigns <paulabenitez808@icloud.com>
2026-07-26 11:13:04 -0400

    Change rotation angle from 150deg to 250deg

--- src/components/liora/BotanicalBg.tsx
+++ src/components/liora/BotanicalBg.tsx
@@ -31,7 +31,7 @@ export function BotanicalBg({ className = "" }: { className?: string }) {
         -left-[-100px]
         top-[350px]
         w-[700px]
-        rotate-[150deg]
+        rotate-[250deg]
         opacity-20
         select-none
       "
