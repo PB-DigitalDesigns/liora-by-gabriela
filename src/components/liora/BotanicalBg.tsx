ab6fc1d83e70f089dd82badd7d52c35731ca2ec2
PB-DigitalDesigns <paulabenitez808@icloud.com>
2026-07-26 11:23:18 -0400

    Adjust top position of Botanical background element

--- src/components/liora/BotanicalBg.tsx
+++ src/components/liora/BotanicalBg.tsx
@@ -29,7 +29,7 @@ export function BotanicalBg({ className = "" }: { className?: string }) {
         hidden lg:block
         absolute
         -left-[-200px]
-        top-[450px]
+        top-[250px]
         w-[700px]
         rotate-[130deg]
         opacity-20
